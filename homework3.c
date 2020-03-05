de0a6aa6c326f2ecef8b05fdfcfe82756f45e69d
candrew9 <candrew9@vt.edu>
2020-03-05 17:25:53 -0500

    Hw3

--- homework3.c
+++ homework3.c
@@ -3,8 +3,13 @@
 #include "myGPIO.h"
 #include "myTimer.h"
 
+
+
+
+
 int main(void)
 {
+    button_enum boardButton = Released;
     // Count variables to control the LEDs.
     unsigned int count0 = 0;
     unsigned int count1 = 0;
@@ -15,13 +20,13 @@ int main(void)
     // Stops the Watchdog timer.
     initBoard();
     // Initialize the GPIO.
-    // YOU MUST WRITE THIS FUNCTION IN myGPIO.c
+
     initGPIO();
     // Initialize Timer0 to provide a one second count interval for updating LED2.
-    // YOU MUST WRITE THIS FUNCTION IN myTimer.c
+
     initTimer(TIMER32_0_BASE, TIMER0_PRESCALER, TIMER0_COUNT);
     // Initialize Timer1 to provide a one millisecond count interval for updating the button history.
-    // YOU MUST WRITE THIS FUNCTION IN myTimer.c
+
     initTimer(TIMER32_1_BASE, TIMER1_PRESCALER, TIMER1_COUNT);
 
     while(1)
@@ -33,19 +38,22 @@ int main(void)
         // YOU MUST WRITE THIS FUNCTION BELOW.
         changeBoosterpackLED(count1);
 
-        // TODO: If Timer0 has expired, increment count0.
+
         // YOU MUST WRITE timer0expired IN myTimer.c
+        if (timer0Expired()) {
+            count0++;
+        }
 
 
-
-        // TODO: If Timer1 has expired, update the button history from the pushbutton value.
         // YOU MUST WRITE timer1expired IN myTimer.c
-
+        if (timer1Expired()) {
+            count1++;
+        }
 
 
         // TODO: Call the button state machine function to check for a completed, debounced button press.
         // YOU MUST WRITE THIS FUNCTION BELOW.
-
+        fsmBoosterpackButtonS1(boardButton);
 
 
         // TODO: If a completed, debounced button press has occurred, increment count1.
@@ -58,28 +66,174 @@ int main(void)
 void initBoard()
 {
     WDT_A_hold(WDT_A_BASE);
+
+    //initialize LEDs
 }
+
+//change to
 
 // TODO: Map the value of a count variable to a color for LED2.
 // Since count is an unsigned integer, you can mask the value in some way.
 void changeLaunchpadLED2(unsigned int count)
 {
 
+    switch(count)
+    {
+    case 1: //change color to red
+        turnOn_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    case 2: //change color to red + green
+        turnOn_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    case 3: //change color to green
+        turnOff_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    case 4: //change color to green + blue
+        turnOff_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    case 5: //change color to blue
+        turnOff_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    case 6: //change color to blue + red
+        turnOn_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    case 7: //change color to red + green + blue
+        turnOn_LaunchpadLED2Red();
+        turnOn_LaunchpadLED2Green();
+        turnOn_LaunchpadLED2Blue();
+        break;
+    default: //no color
+        turnOff_LaunchpadLED2Red();
+        turnOff_LaunchpadLED2Green();
+        turnOff_LaunchpadLED2Blue();
+        break;
+    }
 }
 
 // TODO: Maybe the value of a count variable to a color for the Boosterpack LED
 // This is essentially a copy of the previous function, using a different LED
 void changeBoosterpackLED(unsigned int count)
 {
-
+    switch(count)
+        {
+        case 1: //change color to red
+            turnOn_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        case 2: //change color to red + green
+            turnOn_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        case 3: //change color to green
+            turnOff_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        case 4: //change color to green + blue
+            turnOff_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 5: //change color to blue
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 6: //change color to blue + red
+            turnOn_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 7: //change color to red + green + blue
+            turnOn_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        default: //no color
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        }
 }
 
 // TODO: Create a button state machine.
 // The button state machine should return true or false to indicate a completed, debounced button press.
-bool fsmBoosterpackButtonS1(unsigned int buttonhistory)
-{
+bool fsmBoosterpackButtonS1(button_enum buttonState) {
     bool pressed = false;
+
+
+    //determine the current state
+    unsigned char currentState = GPIO_getInputPinValue(GPIO_PORT_P5,GPIO_PIN1);
+    //unsigned char nextState = 0x00;
+
+
+    switch (buttonState) {
+        case Pressed: //pressed
+            if (currentState == 0) {
+                //button is pressed
+                pressed = true;
+            }
+            else {
+                buttonState = PressedtoReleased;
+            }
+            break;
+        case Released: //released
+            if (currentState == 1) {
+                //button is pressed
+                pressed = false;
+            }
+            else {
+                buttonState = ReleasedtoPressed;
+            }
+
+            break;
+        case PressedtoReleased:
+            //check for timer
+            if (timer1Expired) {
+                if (currentState == 0) {
+                    buttonState = Pressed;
+
+                }
+                else {
+                    buttonState = Released;
+                }
+            }
+            break;
+        case ReleasedtoPressed:
+            //check for timer
+            if (timer1Expired) {
+                if (currentState == 0) {
+                    buttonState = Pressed;
+                }
+                else {
+                    buttonState = Released;
+                }
+            }
+            break;
+
+        }
+
+
+
+
 
 
     return pressed;
 }
+
+
--- homework3.h
+++ homework3.h
@@ -10,6 +10,8 @@
 #ifndef HOMEWORK3_H_
 #define HOMEWORK3_H_
 
+typedef enum {Pressed, PressedtoReleased, ReleasedtoPressed, Released} button_enum;
+
 // This function initializes the board by turning off the Watchdog Timer.
 void initBoard();
 // This function indexes a count value to an LED color on Launchpad LED2.
@@ -17,7 +19,7 @@ void changeLaunchpadLED2(unsigned int count);
 // This function indexes a count value to an LED color on the Boosterpack LED.
 void changeBoosterpackLED(unsigned int count);
 // This function serves as a debounce state machine for Boosterpack button S1
-bool fsmBoosterpackButtonS1(unsigned int buttonhistory);
+bool fsmBoosterpackButtonS1(button_enum buttonState);
 
 
 // TODO: Define any constants that are local to homework.c using #define
